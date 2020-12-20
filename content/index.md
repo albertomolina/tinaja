d2623334403309570caaed6703e278ad69e392bf
Alberto Molina Coballes <alb.molina@gmail.com>
2020-12-20 11:58:14 +0100

    Commit inicial

---
+++ content/index.md
@@ -0,0 +1,10 @@
+title: Tinaja
+slug: index
+date: 2020-12-20
+save_as: index.html
+
+## Contactos
+
+## Correo electrónico
+
+## Redes Sociales
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Alberto Molina Coballes'
+SITENAME = 'Tinaja'
+SITEURL = 'http://tinaja.es'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Madrid'
+
+DEFAULT_LANG = 'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 1
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+RELATIVE_URLS = True
+
+THEME = "./medius/"
+
+DISPLAY_PAGES_ON_MENU = False
+
+# Whether to display categories on the menu of the template.                                  
+DISPLAY_CATEGORIES_ON_MENU = False
